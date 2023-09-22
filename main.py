8c9ebb943f37fa76e2a3e9b87766825347ece1ac
LetMeFly666 <814114971@qq.com>
2023-09-22 21:37:18 +0800

    v2.5 Half不好用版本。解决了切出时视频暂停的问题，但引入了新问题：视频播放完毕后暂停会被重新播放（从前面开始播放），之前判断视频是否播放完毕的方法失效

--- main.py
+++ main.py
@@ -2,81 +2,7 @@
 Author: LetMeFly
 Date: 2021-11-01 15:52:32
 LastEditors: LetMeFly
-LastEditTime: 2023-06-26 13:21:17
-'''
-
-'''
-PS F:\OtherApps\Program\VsCode\Code> & F:/OtherApps/Program/Python/Python/python.exe f:/OtherApps/Program/VsCode/Code/YuKeTang.py
-
-DevTools listening on ws://127.0.0.1:59227/devtools/browser/1345bf1f-6b58-47d7-98cb-20e955bbe99a
-请扫码登陆
-[8224:6516:1102/113245.817:ERROR:chrome_browser_main_extra_parts_metrics.cc(230)] crbug.com/1216328: Checking Bluetooth availability started. Please report if there is no report that this ends.
-[8224:21044:1102/113245.819:ERROR:device_event_log_impl.cc(214)] [11:32:45.820] USB: usb_device_handle_win.cc:1048 Failed to read descriptor from node connection: 连到系统上的设备没有发挥作用。 (0x1F)
-[8224:6516:1102/113245.820:ERROR:chrome_browser_main_extra_parts_metrics.cc(233)] crbug.com/1216328: Checking Bluetooth availability ended.
-[8224:6516:1102/113245.861:ERROR:chrome_browser_main_extra_parts_metrics.cc(236)] crbug.com/1216328: Checking default browser status started. Please report if there is no report that this ends.
-[8224:6516:1102/113245.893:ERROR:chrome_browser_main_extra_parts_metrics.cc(240)] crbug.com/1216328: Checking default browser status ended.
-登录成功
-此课程已完成
-此课程已完成
-此课程已完成
-不是课程
-此课程已完成
-不是课程
-此课程已完成
-不是课程
-此课程已完成
-此课程已完成
-此课程已完成
-不是课程
-不是课程
-此课程已完成
-此课程已完成
-此课程已完成
-[12280:19080:1102/113441.896:ERROR:gpu_init.cc(453)] Passthrough is not supported, GL is disabled, ANGLE is 
-不是课程
-不是课程
-此课程已完成
-此课程已完成
-此课程已完成
-不是课程
-不是课程
-此课程已完成
-此课程已完成
-此课程已完成
-不是课程
-此课程已完成
-此课程已完成
-此课程已完成
-不是课程
-此课程已完成
-此课程已完成
-此课程已完成
-不是课程
-是课程
-暂停状态
-正在播放
-此课程播放完毕！
-此课程已完成
-是课程
-暂停状态
-正在播放
-此课程播放完毕！
-不是课程
-是课程
-暂停状态
-正在播放
-此课程播放完毕！
-是课程
-暂停状态
-正在播放
-此课程播放完毕！
-是课程
-暂停状态
-正在播放
-此课程播放完毕！
-不是课程
-不是课程
-全部完成！
+LastEditTime: 2023-09-12 19:27:08
 '''
 
 from selenium import webdriver
@@ -99,7 +25,7 @@ print("登录成功")
 # 完成一门课程的函数
 def finishThisCourse(courseId):
     # 访问此视频
-    driver.get("https://grsbupt.yuketang.cn/pro/lms/84eubUaed9T/16809342/video/" + courseId)
+    driver.get("https://grsbupt.yuketang.cn/pro/lms/84eubUXLHEy/17556639/video/" + courseId)
     # 判断是否已经完成
     sleep(3)
     if len(driver.find_elements_by_class_name("icon--gou")):
@@ -145,23 +71,39 @@ def finishThisCourse(courseId):
     #         #         print("执行失败")
     #         sleep(3)
     #     sleep(1)
-    driver.execute_script("setInterval(function() {if (document.querySelector('video').paused) {document.querySelector('video').play();}}, 200);")
+
+    driver.execute_script("video = document.querySelector('video');")
+    element_video = driver.find_element_by_tag_name('video')
+    webdriver.ActionChains(driver).move_to_element(element_video).perform()
+    webdriver.ActionChains(driver).click().perform()
+    driver.execute_script("startVideo = setInterval(() => {video.play(); console.log('尝试播放视频');}, 1000); setTimeout(() => {clearInterval(startVideo); console.log('停止尝试播放视频')}, 5200);")
     print("正在播放")
-    while not driver.execute_script("function ifFinished() {percent = document.querySelector('.xt_video_player_progress_currentTime');style = percent.getAttribute('style');return style=='width: 100%;'} return ifFinished();"):
+    driver.execute_script("video.addEventListener('pause', function() {video.play();});")
+    sleep(5)
+    element_speeds = driver.find_element_by_class_name('xt_video_player_common_value')
+    webdriver.ActionChains(driver).move_to_element(element_speeds).perform()
+    sleep(0.2)
+    element_speed2 = driver.find_element_by_xpath('//*[@id="video-box"]/div/xt-wrap/xt-controls/xt-inner/xt-speedbutton/xt-speedlist/ul/li[1]')
+    webdriver.ActionChains(driver).move_to_element_with_offset(element_speed2, 3, 5).perform()
+    sleep(0.3)
+    element_speed2.click()
+    # webdriver.ActionChains(driver).context_click(element_speed2).perform()
+
+    while not len(driver.find_elements_by_class_name("icon--gou")):
         sleep(1)
     print("此课程播放完毕！")
     return True
         
 
 # 访问每一门课
-while True:
-    try:
-        for courseId in range(35657455, 35657509 + 1):
-            finishThisCourse(str(courseId))
-        print("全部完成！")
-        break
-    except:
-        pass
+for courseId in range(37551787, 37551907 + 1):
+    # try:
+    if True:
+        finishThisCourse(str(courseId))
+    # except:
+    #     pass
+print("全部完成！")
+
 
 
 # 雨课堂字幕：https://buct.yuketang.cn/mooc-api/v1/lms/service/subtitle_parse/?c_d=0D0C4F3DE7D58B649C33DC5901307461&lg=0&_=1665473764724
